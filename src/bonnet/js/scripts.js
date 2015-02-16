beb76a92d8662353372b61aad15cff750c6c3aa9
Kyle Conkright <kyleconkright@gmail.com>
2015-02-16 11:46:57 -0800

    Contact Us
    Form working but to email address needs to be same as host.

--- src/bonnet/js/scripts.js
+++ src/bonnet/js/scripts.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    console.log('jQuery Success');
 
 
     $('#masthead').slick({
@@ -18,30 +17,42 @@ $(document).ready(function() {
     });
 
 
+    $(".button").on('click', function(e) {
 
-    // $.ajax({
-    //     url: "./data/data.json",
-    //     type: 'GET',
-    //     dataType: 'JSON',
-    //     success: function(results) {
-    //     console.log('Ajax Success')
-    //
-    //         $('#masthead').slick({
-    //             autoplay: true,
-    //             dots: true,
-    //             pauseOnDotsHover: true,
-    //             arrows: false,
-    //             swipe: false,
-    //             responsive: [{
-    //                 breakpoint: 1024,
-    //                 settings: {
-    //                     swipe: true,
-    //                     dots: false
-    //                 }
-    //             }]
-    //         });
-    //     }
-    // });
+        var name = $("input#name").val();
+        if (name == "") {
+            $("input#name").focus();
+            return false;
+        }
+        var email = $("input#email").val();
+        if (email == "") {
+            $("input#email").focus();
+            return false;
+        }
+        var phone = $("input#phone").val();
+        if (phone == "") {
+            $("input#phone").focus();
+            return false;
+        }
+        var dataString = $('form[name="contact"]').serialize();
+        // alert (dataString);return false;
+        $.ajax({
+            type: "POST",
+            url: "../contact/mail.php",
+            data: dataString,
+            success: function() {
+                $('#contact_form').html("<div id='message'></div>");
+                $('#message').html("<h2>Contact Form Submitted!</h2>")
+                .append("<p>We will be in touch soon.</p>")
+                .hide()
+                .fadeIn(1500, function() {
+                    $('#message').append("<img id='checkmark' src='images/check.png' />");
+                });
+            }
+        });
+        e.preventDefault();
+    });
+
 
 
 });
---
+++ stage/contact/mail.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$message = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.";
+$message .= " Nam iaculis pede ac quam. Etiam placerat suscipit nulla.";
+$message .= " Maecenas id mauris eget tortor facilisis egestas.";
+$message .= " Praesent ac augue sed enim aliquam auctor. Ut dignissim ultricies est.";
+$message .= " Pellentesque convallis tempor tortor. Nullam nec purus.";
+
+// make sure each line doesn't exceed 70 characters
+$message = wordwrap($message, 70);
+
+$subject = $_POST['subject'];
+
+// send email
+mail('kyleconkright@gmail.com', $subject, $message);
+?>
